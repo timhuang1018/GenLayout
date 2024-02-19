d4415125e5785e7e86cbdd065c1b0fd8c0885fc7
Tim Huang <t8522192@gmail.com>
2024-02-19 17:10:22 +0800

    add desktop internet config

--- composeApp/src/desktopMain/kotlin/tim/huang/genlayout/data/network/HttpClient.kt
+++ composeApp/src/desktopMain/kotlin/tim/huang/genlayout/data/network/HttpClient.kt
@@ -1,11 +1,18 @@
 package tim.huang.genlayout.data.network
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.engine.java.*
+
 
 actual fun createHttpClient(): WrappedHttpClient {
     return object : WrappedHttpClient {
-        override suspend fun greeting(): String {
-            TODO("Not yet implemented")
-        }
 
+        val client = HttpClient(Java)
+
+        override suspend fun greeting(): String {
+            return client.get("https://ktor.io/docs/").bodyAsText()
+        }
     }
 }
