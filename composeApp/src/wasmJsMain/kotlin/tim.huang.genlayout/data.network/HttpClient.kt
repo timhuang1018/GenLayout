3ed6d90d4e50f649e9e8c70c84664f62d5357d3a
Tim Huang <t8522192@gmail.com>
2024-02-23 10:11:51 +0800

    wasm internet config, and commonMain internet config

--- composeApp/src/wasmJsMain/kotlin/tim.huang.genlayout/data.network/HttpClient.kt
+++ composeApp/src/wasmJsMain/kotlin/tim.huang.genlayout/data.network/HttpClient.kt
@@ -1,9 +1,16 @@
 package tim.huang.genlayout.data.network
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+
 actual fun createHttpClient(): WrappedHttpClient {
+
+    val client = HttpClient()
+
     return object : WrappedHttpClient {
         override suspend fun greeting(): String {
-            TODO("Not yet implemented")
+            return client.get("https://65d7fb0727d9a3bc1d7bf88e.mockapi.io/api/v1/temp").bodyAsText()
         }
 
     }
