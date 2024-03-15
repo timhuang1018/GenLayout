dd3393fb450e5acbf6b4e9291f2e248590e9c843
Tim Huang <t8522192@gmail.com>
2024-03-15 17:07:48 +0800

    add jwt decoder

---
+++ composeApp/src/commonMain/kotlin/tim/huang/genlayout/utils/Decoder.kt
@@ -0,0 +1,22 @@
+package tim.huang.genlayout.utils
+
+import io.ktor.utils.io.core.String
+import kotlin.io.encoding.Base64.Default.decode
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+@Throws(IllegalArgumentException::class)
+fun decodeJwt(token: String): Pair<String, String> {
+    val parts = token.split(".")
+    if (parts.size != 3) throw IllegalArgumentException("Invalid JWT token.")
+
+    val header = decodeByBase64(parts[0])
+    val payload = decodeByBase64(parts[1])
+
+    return Pair(header, payload)
+}
+
+@OptIn(ExperimentalEncodingApi::class)
+fun decodeByBase64(input: String): String {
+    val byteArray = decode(input)
+    return String(byteArray)
+}
